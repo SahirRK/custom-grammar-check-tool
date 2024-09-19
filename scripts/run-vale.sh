364aed3079fc3aae1d6e60581afcd3b3ce466105
Sahir <sahirk2003@gmail.com>
2024-09-19 10:53:53 -0400

    updated run-vale.sh

--- scripts/run-vale.sh
+++ scripts/run-vale.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker-compose run --rm vale "$@"
+cd "$(dirname "$0")/.." || exit
+
+run_docker_compose() {
+    docker-compose run --rm vale "$@"
+}
+
+echo "Syncing Vale packages ..."
+run_docker_compose sync
+
+echo "Running Vale ..."
+run_docker_compose "$@"
